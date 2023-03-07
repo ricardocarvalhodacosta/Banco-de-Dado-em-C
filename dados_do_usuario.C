c967da833740d68b0b256bd28d019003e76824a4
RIcardo Carvalho da Costa <115588683+rRickers@users.noreply.github.com>
2023-03-07 18:27:56 -0300

    Testando bibliotecas
    Hoje comecei a testar as bibliotecas e possíveis soluções para a criação deste banco de dados. Consegui criar um arquivo para armazenar dados, mas ainda não encontrei o método correto para navegar através dos arquivos e criar novos arquivos por um menu.

--- dados_do_usuario.C
+++ dados_do_usuario.C
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    FILE *file;
+    char nome[20], c;
+
+    printf("Nome: ");
+    gets(nome);
+
+    file = fopen("%s" "r", nome);
+
+    if (file == NULL)
+    {
+        printf("Nao foi possivel abrir o arquivo.\n");
+        getchar();
+        exit(0);
+    }
+
+    char frase[100];
+
+    while (fgets(frase, 100, file) != NULL)
+    {
+        printf("%s", frase);
+    }
     
-    return 0;
+    fclose(file);
+
+    printf("\n");
+    system("pause");
+    return(0);
 }
