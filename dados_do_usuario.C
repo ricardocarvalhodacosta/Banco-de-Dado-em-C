48efb8d7e6562168819184b03412c8944df5aedb
RIcardo Carvalho da Costa <115588683+rRickers@users.noreply.github.com>
2023-03-08 20:46:58 -0300

    Testando a função switch
    Testei hoje de diferentes maneiras a função switch para a criação do banco de dados, infelizmente ainda não tivesse o sucesso que desejo.

--- dados_do_usuario.C
+++ dados_do_usuario.C
@@ -1,33 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(void)
 {
-    FILE *file;
-    char nome[20], c;
+    FILE *arquivo;
+    char cliente;
+    char emailcliente;
+    char telefonecliente;
+    int menu;
 
-    printf("Nome: ");
-    gets(nome);
+    printf("Bem-vindo ao banco de dados em C;\n");
+    printf("O que voce gostaria de fazer?\n");
+    printf("1. Adicionar um novo usuario;\n");
+    printf("2. Verificar um usuario existente;\n");
+    printf("3. Modificar um usuario existente;\n");
+    printf("Digite o numeral correspondente: ");
+    menu = getche();
+    printf("\n");
 
-    file = fopen("%s" "r", nome);
-
-    if (file == NULL)
+    switch (menu)
     {
-        printf("Nao foi possivel abrir o arquivo.\n");
-        getchar();
-        exit(0);
-    }
+    case '1':
+        printf("Qual o nome do cliente? \n");
+        scanf("%s", cliente);
+        arquivo = fopen (arquivo, "%s", cliente, "w");
+        printf("E-mail do cliente: \n");
+        scanf("%s", emailcliente);
+        fprintf("%s", emailcliente);
+        printf("Telefone cliente: \n");
+        scanf("%s", telefonecliente);
+        fprintf("%s", telefonecliente);
+        printf("Dados coletados e armazenados com sucesso! \n\n");
+        fclose(arquivo);
+        break;
+    case '2':
+        printf("Qual o nome do cliente? \n");
+        scanf("%s", cliente);
+        arquivo = fopen (arquivo, "%s", cliente, "r");
+        printf(arquivo, "Dados do cliente: \n\n");
+        system("PAUSE");
+        fclose(arquivo);
+        break;
+    case '3':
+        printf("Qual o nome do cliente? \n");
+        scanf("%s", cliente);
+        arquivo = fopen (arquivo, "%s", cliente, "a");
+        printf(arquivo, "Dados do cliente: \n\n");
+        printf("O que gostaria de mudar: Nome, E-mail, Telefone? \n");
+        printf("Digite a primeira letra do objeto: N (nome), E(e-mail), T(telefone)\n");
+        menu = getche();
+        switch (menu)
+        {
+        case 'N':
+            printf("Digite o novo nome: \n");
+            scanf("%s", cliente);
+            fwrite(arquivo, "%s", cliente);
+            fclose(arquivo);
+            break;
+        case 'E':
+            printf("Digite o novo E-mail: \n");
+            scanf("%s", emailcliente);
+            fwrite(arquivo, "%s", emailcliente);
+            fclose(arquivo);
+            break;
+        case 'N':
+            printf("Digite o novo telefone: \n");
+            scanf("%s", telefonecliente);
+            fwrite(arquivo, "%s", telefonecliente);
+            fclose(arquivo);
+            break;
+        default:
+            break;
+        }
 
-    char frase[100];
+        system("PAUSE");
+        fclose(arquivo);
 
-    while (fgets(frase, 100, file) != NULL)
-    {
-        printf("%s", frase);
+    default:
+        break;
     }
     
-    fclose(file);
-
-    printf("\n");
-    system("pause");
+    system("PAUSE");
     return(0);
 }
