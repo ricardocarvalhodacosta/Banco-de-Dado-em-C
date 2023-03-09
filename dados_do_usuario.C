320fa6fac48dd3966ebd73691b1797ab63086086
RIcardo Carvalho da Costa <115588683+rRickers@users.noreply.github.com>
2023-03-09 20:45:31 -0300

    Primeiras funções funcionando
    Consegui criar as primeiras funções para o banco de dados, utilizando funções e if else;
    Também foi adicionado a opção no menu de verificar os dados do cliente.

--- dados_do_usuario.C
+++ dados_do_usuario.C
@@ -2,84 +2,76 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(void)
-{
+void menu() {
+
+    int i;
+
+    printf("Ola, eu sou bancoC.txt!\n");
+    printf("No que eu posso ser util hoje?\n");
+    printf("1. Adicionar Cliente; 2. Verificar um cliente;\n\n");
+
+    scanf("%i", &i);
+
     FILE *arquivo;
-    char cliente;
-    char emailcliente;
-    char telefonecliente;
-    int menu;
 
-    printf("Bem-vindo ao banco de dados em C;\n");
-    printf("O que voce gostaria de fazer?\n");
-    printf("1. Adicionar um novo usuario;\n");
-    printf("2. Verificar um usuario existente;\n");
-    printf("3. Modificar um usuario existente;\n");
-    printf("Digite o numeral correspondente: ");
-    menu = getche();
-    printf("\n");
-
-    switch (menu)
-    {
-    case '1':
-        printf("Qual o nome do cliente? \n");
-        scanf("%s", cliente);
-        arquivo = fopen (arquivo, "%s", cliente, "w");
-        printf("E-mail do cliente: \n");
-        scanf("%s", emailcliente);
-        fprintf("%s", emailcliente);
-        printf("Telefone cliente: \n");
-        scanf("%s", telefonecliente);
-        fprintf("%s", telefonecliente);
-        printf("Dados coletados e armazenados com sucesso! \n\n");
-        fclose(arquivo);
-        break;
-    case '2':
-        printf("Qual o nome do cliente? \n");
-        scanf("%s", cliente);
-        arquivo = fopen (arquivo, "%s", cliente, "r");
-        printf(arquivo, "Dados do cliente: \n\n");
-        system("PAUSE");
-        fclose(arquivo);
-        break;
-    case '3':
-        printf("Qual o nome do cliente? \n");
-        scanf("%s", cliente);
-        arquivo = fopen (arquivo, "%s", cliente, "a");
-        printf(arquivo, "Dados do cliente: \n\n");
-        printf("O que gostaria de mudar: Nome, E-mail, Telefone? \n");
-        printf("Digite a primeira letra do objeto: N (nome), E(e-mail), T(telefone)\n");
-        menu = getche();
-        switch (menu)
+        if (i==1)
         {
-        case 'N':
-            printf("Digite o novo nome: \n");
-            scanf("%s", cliente);
-            fwrite(arquivo, "%s", cliente);
-            fclose(arquivo);
-            break;
-        case 'E':
-            printf("Digite o novo E-mail: \n");
-            scanf("%s", emailcliente);
-            fwrite(arquivo, "%s", emailcliente);
-            fclose(arquivo);
-            break;
-        case 'N':
-            printf("Digite o novo telefone: \n");
-            scanf("%s", telefonecliente);
-            fwrite(arquivo, "%s", telefonecliente);
-            fclose(arquivo);
-            break;
-        default:
-            break;
-        }
+            char nome[50];
+            char email[200];
+            char telefone[30];
 
-        system("PAUSE");
+            printf("Digite o nome do cliente: \n");
+            scanf("%s", nome);
+            printf("\nDigite o email do cliente: \n");
+            scanf("%s", email);
+            printf("\nDigite o telefone do cliente: \n");
+            scanf("%s", telefone);
+
+            arquivo = fopen ("bancoC.txt", "w");
+            printf("Arquivo aberto com sucesso! Salvando dados...\n\n");
+            fprintf(arquivo, "Nome: %s | E-mail: %s | Telefone: %s", nome, email, telefone);
+
+            if (arquivo == NULL)
+            {
+                printf("Nao foi possivel abrir o arquivo!\n\n");
+                system("PAUSE");
+            } else {
+                printf("\n\nDados salvados com sucesso!\n\n");
+            }
+
+            fclose(arquivo);
+        }
+        else
+        {
+            char leitura;
+
+            arquivo = fopen ("bancoC.txt", "r");
+            if (arquivo == NULL)
+            {
+                printf("Nao foi possivel abrir o arquivo!\n\n");
+                system("PAUSE");
+            } else {
+                printf("\n\nLendo os dados... Eles sao: \n\n");
+            }
+
+            do
+            {
+                leitura = fgetc(arquivo);
+                printf("%c", leitura);
+
+            } while (leitura != EOF);
+
         fclose(arquivo);
 
-    default:
-        break;
     }
+
+}
+
+int main()
+{
+
+    menu();
+    printf("\n\n");
     
     system("PAUSE");
     return(0);
